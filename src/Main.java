0b314a15e6b8b3111b789204014dbd841bec20be
Chirkin Ivan <fruitin3d@yandex.ru>
2022-08-23 09:52:25 +0700

    feat: добавить пример работы в Main

--- src/Main.java
+++ src/Main.java
@@ -11,26 +11,32 @@ public class Main {
         TaskManager manager = Managers.getDefault();
         manager.addTask(new Task("task1","good description", Status.NEW));
         manager.addTask(new Task("task2","very good description", Status.IN_PROGRESS));
-        manager.addTask(new EpicTask("Epic1", "very epic description"));
-        manager.addTask(new Subtask("Sub1", "desc1", Status.NEW, 2));
-        manager.addTask(new Subtask("Sub2", "desc2", Status.IN_PROGRESS, 2));
+        int epicId = manager.addTask(new EpicTask("Epic1", "very epic description"));
+        manager.addTask(new Subtask("Sub1", "desc1", Status.NEW, epicId));
+        manager.addTask(new Subtask("Sub2", "desc2", Status.IN_PROGRESS, epicId));
+        manager.addTask(new Subtask("Sub3", "desc3", Status.IN_PROGRESS, epicId));
         manager.addTask(new EpicTask("Epic2", "epic description"));
-        manager.addTask(new Subtask("Sub3", "desc3", Status.NEW, 5));
-        System.out.println(manager.getHistory());
         manager.getTaskById(0);
         System.out.println(manager.getHistory());
         manager.getEpicTaskById(2);
         System.out.println(manager.getHistory());
         manager.getSubtaskById(3);
         System.out.println(manager.getHistory());
+        manager.getSubtaskById(1);
+        System.out.println(manager.getHistory());
+        manager.getSubtaskById(4);
+        System.out.println(manager.getHistory());
+        manager.getSubtaskById(6);
+        System.out.println(manager.getHistory());
+        manager.getSubtaskById(3);
+        System.out.println(manager.getHistory());
         manager.getEpicTaskById(5);
         System.out.println(manager.getHistory());
-        //заполение истории
-        for (int i = 0; i < 6; i++) {
-            manager.getSubtaskById(6);
-        }
+        manager.getEpicTaskById(2);
         System.out.println(manager.getHistory());
-        manager.getTaskById(1);
+        manager.removeTaskById(3);
+        System.out.println(manager.getHistory());
+        manager.removeTaskById(epicId);
         System.out.println(manager.getHistory());
     }
 }
