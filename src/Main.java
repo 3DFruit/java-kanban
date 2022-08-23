c03e56d6928b16f499cd9d6ee63500419e4f3e95
Chirkin Ivan <fruitin3d@yandex.ru>
2022-08-23 10:52:40 +0700

    refactor: убрать неиспользуемые импорты

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         TaskManager manager = Managers.getDefault();
-        manager.addTask(new Task("task1","good description", Status.NEW));
-        manager.addTask(new Task("task2","very good description", Status.IN_PROGRESS));
+        manager.addTask(new Task("task1", "good description", Status.NEW));
+        manager.addTask(new Task("task2", "very good description", Status.IN_PROGRESS));
         int epicId = manager.addTask(new EpicTask("Epic1", "very epic description"));
         manager.addTask(new Subtask("Sub1", "desc1", Status.NEW, epicId));
         manager.addTask(new Subtask("Sub2", "desc2", Status.IN_PROGRESS, epicId));
--- src/managers/history/Node.java
+++ src/managers/history/Node.java
@@ -5,9 +5,9 @@ public class Node<T> {
     public Node<T> prev;
     public T data;
 
-    public Node (Node<T> prev, T data, Node<T> next) {
+    public Node(Node<T> prev, T data, Node<T> next) {
         this.next = next;
         this.prev = prev;
-        this. data = data;
+        this.data = data;
     }
 }
