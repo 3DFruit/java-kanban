8626d3342c757cddc6c4ccefd0b74abf274262bb
Chirkin Ivan <fruitin3d@yandex.ru>
2022-10-06 22:17:56 +0700

    feat: добавить HTTPTaskManagerTest

--- tests/managers/taskmanager/HTTPTaskManagerTest.java
+++ tests/managers/taskmanager/HTTPTaskManagerTest.java
@@ -1,11 +1,29 @@
 package managers.taskmanager;
 
 
+import managers.Managers;
+import managers.servers.KVServer;
+import org.junit.jupiter.api.AfterEach;
+import java.io.IOException;
+
 class HTTPTaskManagerTest extends TaskManagerTest<HTTPTaskManager> {
 
-
+    KVServer server;
     @Override
     protected HTTPTaskManager createManager() {
+        try {
+            server = new KVServer();
+            server.start();
+            return (HTTPTaskManager) Managers.getDefault();
+        }
+        catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    @AfterEach
+    void stopServer() {
+        server.stop(1);
     }
 }
